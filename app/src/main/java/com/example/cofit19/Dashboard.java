a1444459bcf8fa8699cecf4373394e41ff5bdd90
rivaldofez <rivaldofez@gmail.com>
2020-05-03 18:28:34 +0700

    menambahkan main exercise dan dashboard

---
+++ app/src/main/java/com/example/cofit19/Dashboard.java
@@ -0,0 +1,17 @@
+package com.example.cofit19;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+    }
+}
